d8320f2a8f40b578af685ce5ef3b0bb89729d22e
Aoi-U <aoilovepokemon@gmail.com>
2024-11-21 12:15:49 -0700

    classes/constants in separate file, solution.py renamed

--- startState.py
+++ startState.py
@@ -1,57 +1,7 @@
 from parser import parser, data
 from dataclasses import dataclass
+from constants import *
 import sys
-
-MO = 'MO'
-TU = 'TU'
-FR = 'FR'
-"""
-    Dictionary to map time strings to integers
-    If there is a simpler way to do this, please lmk
-"""
-TIME = {
-    "8:00": 0, "8:30": 1, "9:00": 2, "9:30": 3,"10:00": 4,
-    "10:30": 5, "11:00": 6, "11:30": 7, "12:00": 8, "12:30": 9,
-    "13:00": 10, "13:30": 11, "14:00": 12, "14:30": 13, "15:00": 14,
-    "15:30": 15, "16:00": 16, "16:30": 17, "17:00": 18, "17:30": 19,
-    "18:30": 21, "19:00": 22, "19:30": 23, "20:00": 24, "20:30": 25, "21:00": 26
-}
-
-"""
-    Slot class to represent a slot in the schedule
-    game: tuple of (list of games, max games, min games)
-    practice: tuple of (list of practices, max practices, min practices)
-"""
-@dataclass
-class Slot:
-    game: tuple[list[data.Game], int, int] = ([], 0, 0)
-    prac: tuple[list[data.Practice], int, int] = ([], 0, 0)
-
-"""
-    Schedule class to represent the schedule
-    mo: list of slots for monday
-    tu: list of slots for tuesday
-    fr: list of slots for friday
-"""
-@dataclass
-class Schedule:
-    mo: list[Slot] 
-    tu: list[Slot] 
-    fr: list[Slot] 
-
-
-"""
-    Problem class to represent a problem
-    sched: A schedule
-    remGame: list of remaining games
-    remPrac: list of remaining practices
-"""
-@dataclass
-class Problem:
-    sched: Schedule 
-    remGames: list[data.Game] 
-    remPracs: list[data.Practice] 
-
 
 """
     Start state functionality from the project proposal
@@ -93,7 +43,6 @@ def start():
         elif (slot.getDay() == FR):
             pr.sched.fr[TIME[slot.getStartTime()]].prac = ([], slot.getPracticeMax(), slot.getPracticeMin())
     
-
     # Fulfill partial assignments for games
     for game in data.Games.getGames():        
         # Check if there is a partial assignment for the game
