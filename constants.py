d95fdff1961050eb5ba1df50a7432fcc86170a5b
Aoi-U <aoilovepokemon@gmail.com>
2024-11-21 12:17:28 -0700

    added comments

--- constants.py
+++ constants.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass
 from parser import data
 
+"""
+    This file only contains necessary constants and data structures for problem representation
+"""
+
 MO = 'MO'
 TU = 'TU'
 FR = 'FR'
