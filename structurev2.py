2b6cb870d65fdcea7b05830815abf7b22479f0f3
Aoi-U <aoilovepokemon@gmail.com>
2024-11-20 18:21:40 -0700

    add soft constraint descriptions

--- structurev2.py
+++ structurev2.py
@@ -4,15 +4,30 @@
 # Fleaf will choose a leaf with breadth/depth or random selection
 # End result will be the same as V1 but with less computation on calculating leaf values
 
+incompatible = [] # List of incompatible games/practices
+unwanted = [] # List of unwanted games/practices
+preferences = [] # List of preferences for games/practices
+pairs = [] # List of games/practices that want to be scheduled together
+
+"""
+    Node class to store the schedule and remaining games/practices
+    Is equivalent to one pr from the ATree proposal
+"""
+class Node:
+    # Note: Could change depending on how the parser is implemented
+    def __init__(self, schedule, remaining):
+        self.schedule = schedule
+        self.remaining = remaining
+
 """
     Main code loop for AND tree search algorithm
     @param prob: Initial problem state
     @return: Solution to the problem
 """
-def treeSerach(prob):
+def treeSearch(prob):
     # Initially, prob is a list of the initial problem state
-    leafs = []
-    solutions = []
+    leafs = [] # Contains all leaf nodes in the tree
+    solutions = [] # Contains all potential solutions to the problem
 
     # Loop until we cannot generate any more leaves
     while True:
@@ -32,8 +47,8 @@ def treeSerach(prob):
         newLeafs = div(pr, match)
 
         # If no new leafs are generated, pr is a solution
-        # Add pr to solutions 
-        if len(newLeafs) == 0:
+        # Additionally, we will only consider pr as a "real" solution if there are no remaining games/practices
+        if len(newLeafs) == 0 & len(pr.remaining) == 0:
             solutions.append(pr)
         # Add new leafs to leafs
         leafs += newLeafs
@@ -57,7 +72,8 @@ def treeSerach(prob):
     @return: List of new valid schedules (leafs)
 """
 def div(pr, match):
-    newLeafs = []
+    newLeafs = [] # Contains all new leafs that DIV generates
+
     # Look through all open slots in the leaf and check if it is valid or not based on hard constraints HC()
     # If it is valid, generate a new leaf node with match in that slot
     # Append the new leaf to newLeafs
@@ -87,6 +103,8 @@ def fleaf(leafs):
     @return game or practice to schedule
 """
 def ftrans(pr):
+    remaining = pr.remaining 
+
     # Choose a game or practice from preferences
     # If none, choose a game or practice from pairs
     # If none, choose a random game or practice
@@ -105,10 +123,19 @@ def HC(slot, match):
 """
     Calculate soft constraints for all solutions
     @param solutions: List of solutions
-    @return: Best solution
+    @return: Best solution 
 """
 def SC(solutions):
     # Calculate soft constraints for all solutions
     
+    # Loop through all solutions and calculate the soft constraints 
+        # gpmin 
+            # for each slot, the number of games/practices scheduled should be greater than or equal to the minimum 
+        # pref
+            # look at each game/practice from preferences and see if it is scheduled in that time
+        # atgame 
+            # Different divisional games within a single age/tier group should be scheduled at different times
+
+
     # Return the best solution
     return 0
