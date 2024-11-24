35a96794240f3466c4c6fd196f0d957fa6bdeba4
Ray Sandhu <aryamansandhu123@gmail.com>
2024-11-23 18:24:05 -0700

    Updated structure of Prob and using newly optimized methods for game/practice CRUD

--- constants.py
+++ constants.py
@@ -1,47 +1,25 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Dict
 from parserFile import data
 
 """
     This file only contains necessary constants and data structures for problem representation
 """
 
-MO = 'MO'
-TU = 'TU'
-FR = 'FR'
-
-"""
-    Dictionary to map time strings to integers
-    If there is a simpler way to do this, please lmk
-"""
-TIME = {
-    "8:00": 0, "8:30": 1, "9:00": 2, "9:30": 3,"10:00": 4,
-    "10:30": 5, "11:00": 6, "11:30": 7, "12:00": 8, "12:30": 9,
-    "13:00": 10, "13:30": 11, "14:00": 12, "14:30": 13, "15:00": 14,
-    "15:30": 15, "16:00": 16, "16:30": 17, "17:00": 18, "17:30": 19,
-    "18:30": 21, "19:00": 22, "19:30": 23, "20:00": 24, "20:30": 25, "21:00": 26
-}
-
-"""
-    Slot class to represent a slot in the schedule
-    game: tuple of (list of games, max games, min games)
-    practice: tuple of (list of practices, max practices, min practices)
-"""
-@dataclass
-class Slot:
-    game: tuple[list[data.Game], int, int] = ([], 0, 0)
-    prac: tuple[list[data.Practice], int, int] = ([], 0, 0)
-
 """
     Schedule class to represent the schedule
-    mo: list of slots for monday
-    tu: list of slots for tuesday
-    fr: list of slots for friday
+    gameslots: dict with 
+                    key as a date-time string of gameSlots
+                    value as list of games that have been scheduled then
+    practSlots: dict with 
+                    key as a date-time string of practiceSlots
+                    value as list of practices that have been scheduled then
 """
+
 @dataclass
 class Schedule:
-    mo: list[Slot] 
-    tu: list[Slot] 
-    fr: list[Slot] 
+    gameSlots: Dict[str, list[str]] = field(default_factory=dict)
+    pracSlots: Dict[str, list[str]] = field(default_factory=dict)
 
 
 """
--- startState.py
+++ startState.py
@@ -15,103 +15,49 @@ def start():
     # Create the start state 
     pr = Problem(
         sched = Schedule(
-            ## !!! RAY-Q why do we need a full schedule with invalid slots, we already have a list of the possible slots
-            # !! RAY-Q don't see the point of coupling gameslots and practiceslots in the Slot class
-            # Initialize the schedule with empty slots
-            mo = [Slot() for i in range(27)],
-            tu = [Slot() for i in range(27)],
-            fr = [Slot() for i in range(27)]
+            # Initialize the schedule with parsed game and practice slots
+            gameSlots= {key: [] for key in data.GameSlots.gameSlotsDict.keys()},
+            pracSlots= {key: [] for key in data.PracticeSlots.practiceSlotsDict.keys()}
         ),
         # Get the remaining games and practices
         remGames = data.Games.getGames(),
         remPracs = data.Practices.getPractices()
     )
 
-    # !! RAY-Q this becomes unnecessary because only slots with gameMin/Maxes would be in sched
-    # Where there are game slots, set game max and game min in pr to their corresponding values 
-    for slot in data.GameSlots.getGameSlots():
-        if (slot.getDay() == MO):
-            pr.sched.mo[TIME[slot.getStartTime()]].game = ([], slot.getGameMax(), slot.getGameMin())
-        elif (slot.getDay() == TU):
-            pr.sched.tu[TIME[slot.getStartTime()]].game = ([], slot.getGameMax(), slot.getGameMin())
-        elif (slot.getDay() == FR):
-            pr.sched.fr[TIME[slot.getStartTime()]].game = ([], slot.getGameMax(), slot.getGameMin())
-
-    # Where there are practice slots, set practice max and practice min in pr to their corresponding values 
-    for slot in data.PracticeSlots.getPracticeSlots():
-        if (slot.getDay() == MO):
-            pr.sched.mo[TIME[slot.getStartTime()]].prac = ([], slot.getPracticeMax(), slot.getPracticeMin())
-        elif (slot.getDay() == TU):
-            pr.sched.tu[TIME[slot.getStartTime()]].prac = ([], slot.getPracticeMax(), slot.getPracticeMin())
-        elif (slot.getDay() == FR):
-            pr.sched.fr[TIME[slot.getStartTime()]].prac = ([], slot.getPracticeMax(), slot.getPracticeMin())
-    
     # Fulfill partial assignments for games
-    for game in data.Games.getGames():
-        # Check if there is a partial assignment for the game
-        slot = game.getPartialAssignmentSlot()
-        if (slot != None): 
-            day = None
-            time = None
-            # Get the day and time of the partial assignments slot
-            if (slot.getDay() == MO):
-                day = pr.sched.mo
-                time = TIME[slot.getStartTime()] 
-            elif (slot.getDay() == TU):
-                day = pr.sched.tu
-                time = TIME[slot.getStartTime()]
-            else:
-                day = pr.sched.fr
-                time = TIME[slot.getStartTime()]
+    for game in pr.remGames:
+        preferredSlot = game.getPartialAssignmentSlot()
 
-            # Check if the slot is valid using HC()
-            if (HC(day[time].game, game)):
-                # Add the game to the slot
-                day[time].game[0].append(game)
-                # Update the remaining games
+        ## !!! RAY-A this is as simple as adding a game to a slot would be
+        if (preferredSlot):
+            if (HC(pr.sched, preferredSlot.id, game.identifier)):
+                pr.sched.gameSlots[preferredSlot.id].append(game.identifier)
                 pr.remGames.remove(game)
-            # If the slot is invalid, print an error and exit
-            else: 
-                print("Invalid slot for partial assignment; no valid assignment possible with HC")
-                exit(1)
-
-    # Fulfill partial assignments for practices   
-    for prac in data.Practices.getPractices():        
-        # Check if there is a partial assignment for the practice
-        slot = prac.getPartialAssignmentSlot()
-        if (slot != None): 
-            day = None
-            time = None
-            # Get the day and time of the partial assignments slot
-            if (slot.getDay() == MO):
-                day = pr.sched.mo
-                time = TIME[slot.getStartTime()] 
-            elif (slot.getDay() == TU):
-                day = pr.sched.tu
-                time = TIME[slot.getStartTime()]
             else:
-                day = pr.sched.fr
-                time = TIME[slot.getStartTime()]
+                raise print(f"Invalid slot for partial assignment; no valid assignment possible with HC")
 
-            # Check if the slot is valid using HC()
-            if (HC(day[time].prac, prac)):
-                # Add the practice to the slot
-                day[time].prac[0].append(prac)
-                # Update the remaining practices
+    for prac in pr.remPracs:
+        preferredSlot = prac.getPartialAssignmentSlot()
+
+        ## !!! RAY-A this is as simple as adding a prac to a slot would be
+        if (preferredSlot):
+            if (HC(pr.sched, preferredSlot.id, prac.identifier)):
+                pr.sched.pracSlots[preferredSlot.id].append(prac.identifier)
                 pr.remPracs.remove(prac)
             else:
-                print("Invalid slot for partial assignment; no valid assignment possible with HC")
-                exit(1)
+                raise print(f"Invalid slot for partial assignment; no valid assignment possible with HC")
+
     print(pr)
     return pr
 
 """
     Calculate hard constraints on a match for a given slot
-    @param slot: Slot to check
-    @param match: Game/practice to schedule
+    @param sched: Complete schedule to check against
+    @param slotToCheck: The slot to which a game/practice is being added
+    @param match: Specific game/practice that is being added
     @return: True if slot is valid, False otherwise
 """
-def HC(slot, match):
+def HC(sched, slotToCheck, match):
     # Check if slot is valid based on hard constraints
     return True
 
